4e331afa7618aa52e033d4b311ca1d55aa77f09b
HeoGyeongHwan <68696099+hkh1284@users.noreply.github.com>
2022-03-06 22:37:26 +0900

    Create 시저암호.py

---
+++ Etc/Programmers/Practice Problem/시저암호.py
@@ -0,0 +1,6 @@
+#활용 문법
+#str.isupper() : str이 모두 대문자면 True리턴, 아니면 False리턴
+#str.islower() : str이 모두 소문자면 True리턴, 아니면 False리턴
+#cf) str.upper() : str을 모두 대문자로 바꾸어 리턴, str.lower() : str을 모두 소문자로 바꾸어 리턴
+
+#chr() : 아스키코드값을 문자로 바꾸어주는 함수, ord() : 문자를 아스키코드값으로 바꾸어주는 함수
