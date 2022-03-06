47718938e39cf35e6e1cfbe67e0d888adad4ecc0
HeoGyeongHwan <68696099+hkh1284@users.noreply.github.com>
2022-03-06 17:33:50 +0900

    Update and rename find_prime_number.py to is_prime_number.py

--- Number Theory/find_prime_number.py
+++
@@ -1,5 +0,0 @@
-#에라토스테네스의 체
-import math
-
-def find_prime_number(x):
-  
---
+++ Number Theory/is_prime_number.py
@@ -0,0 +1,8 @@
+
+import math
+
+def is_prime_number(x):
+  for i in range(2, int(math.sqrt(x))+1): #
+    if x % i == 0:
+      return False #소수가 아니면 False반환
+  return True #소수면 True반환
